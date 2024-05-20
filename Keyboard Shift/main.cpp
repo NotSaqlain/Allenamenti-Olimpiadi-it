597b7d6b3de53377b57a018afa19aa3321f445df
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-05-20 08:43:30 +0200

    Update main.cpp

--- Keyboard Shift/main.cpp
+++ Keyboard Shift/main.cpp
@@ -19,11 +19,9 @@ int main() {
     // insert your code here
     string keyboard = "qwertyuiopasdfghjklzxcvbnm";
     for (int i = 0; i < S.size(); i++) {
-        int index = keyboard.find(S[i]);
-        S[i] = keyboard[index + 1];
+        int tasto = keyboard.find(S[i]);
+        S[i] = keyboard[tasto + 1];
     }
-
-    // output data
 
     cout << S << endl;
     return 0;
